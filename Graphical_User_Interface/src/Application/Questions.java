2214f80e3d5ea24e7fd3ff84e1082394263601a7
MA7 <amineac7@gmail.com>
2019-04-27 17:12:06 +0200

    [Questions Page Interface]: Errors cases + Answers saves + Interactions

--- Graphical_User_Interface/src/Application/Questions.java
+++ Graphical_User_Interface/src/Application/Questions.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 
 public class Questions {
 	
-	final int SIZE = 15;	//The nb of questions
+	final int SIZE = 11;	//The nb of questions
 	//The content of each question
 	final String qu1 = "[Question 1 Content xxxxxxxxxxxxxxxxxx]" ;
 	final String qu2 = "[Question 2 Content xxxxxxxxxxxxxxxxxx]" ;
@@ -18,32 +18,42 @@ public class Questions {
 	final String qu9 = "[Question 9 Content xxxxxxxxxxxxxxxxxx]" ;
 	final String qu10 = "[Question 10 Content xxxxxxxxxxxxxxxxxx]" ;
 
-	public String tab_qus [] = new String [SIZE] ;	//The question table
+	public String tab_questions [] = new String [SIZE] ;	//The question table
+	public String tab_answers [] = new String [SIZE] ;  	//The answer table
+	//TO CHECK: tab_answers will not necessery only contain String
+
 
 	
 	public Questions() {
-		tab_qus[1] = qu1 ;
-		tab_qus[2] = qu2 ;
-		tab_qus[3] = qu3 ;
-		tab_qus[4] = qu4 ;
-		tab_qus[5] = qu5 ;
-		tab_qus[6] = qu6 ;
-		tab_qus[7] = qu7 ;
-		tab_qus[8] = qu8 ;
-		tab_qus[9] = qu9 ;
-		tab_qus[10] = qu10 ;
+		tab_questions[1] = qu1 ;
+		tab_questions[2] = qu2 ;
+		tab_questions[3] = qu3 ;
+		tab_questions[4] = qu4 ;
+		tab_questions[5] = qu5 ;
+		tab_questions[6] = qu6 ;
+		tab_questions[7] = qu7 ;
+		tab_questions[8] = qu8 ;
+		tab_questions[9] = qu9 ;
+		tab_questions[10] = qu10 ;
 
 	}
 
 	//Get a question content from the question index
-	public String get_qu(int index) {
-		return tab_qus[index] ;
+	public String get_question(int index) {
+		return tab_questions[index] ;
 	}
 	
 	//Get the number of questions
-		public int get_nb_qus() {
-			return SIZE ;
-		}
+	public int get_nb_questions() {
+		return SIZE ;
+	}
+	
+	//Set an answer from the question index
+	public void set_answer(int index, String answer) {
+		tab_answers[index] = answer ;
+
+	}
+		
 		
 	public static void main(String[] args) {
 		
--- Graphical_User_Interface/src/Interface/Interface.java
+++ Graphical_User_Interface/src/Interface/Interface.java
@@ -2,12 +2,15 @@ package Interface;
 
 import Application.Questions ;
 
-
+import java.awt.Dialog;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -15,6 +18,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -28,9 +32,12 @@ import javax.swing.DefaultListModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -47,12 +54,12 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 public class Interface extends JFrame{
 	private static JPanel contentPane;
-	private static JPanel ErrorPane ;
 	static Interface frame;
-	public int num_page = 0 ;
+	public int num_page = 0 ;	//Current question page number
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -72,15 +79,16 @@ public class Interface extends JFrame{
 		
 		//Questions list
 		Questions questions = new Questions() ;		//Initialize the table of questions
-		int nb_questions = questions.get_nb_qus() ;	//Get the nb of questions
+		int nb_questions = questions.get_nb_questions() ;	//Get the nb of questions
 		
 		//Global interface (the windows)
 		setTitle("CeSort");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(200, 100, 900, 600);
+        //setLocationRelativeTo();
+   
 		setResizable(false);
 		contentPane = new JPanel();
-		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout(0, 0));
 		
@@ -90,10 +98,16 @@ public class Interface extends JFrame{
 		welcome_panel.setLayout(null);
 		//welcome_panel.setBackground(Color.WHITE); //Fond blanc comme le logo mais le fond par défaut est mieux
 		
-		//Question 1 page
+		//Questions page
 		JPanel qus_panel = new JPanel();
 		qus_panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		qus_panel.setLayout(null);
+		
+		//Results page
+		JPanel results_panel = new JPanel();
+		results_panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		results_panel.setLayout(null);
+		
 		
 		final CardLayout cardLayout = (CardLayout) contentPane.getLayout();
 		
@@ -149,7 +163,7 @@ public class Interface extends JFrame{
 		//Load Button
 		JButton button_load = new JButton("Load project");
 		button_load.setFont(new Font("Bitstream Charter", Font.BOLD, 25)); 	
-		button_load.setBounds(400, 470, 160,50);	
+		button_load.setBounds(440, 470, 160,50);	
 		button_load.setForeground(Color.WHITE);
 		button_load.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
 		button_load.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -233,7 +247,6 @@ public class Interface extends JFrame{
         jlanguaget.setOpaque(true) ;
 		welcome_panel.add(jlanguaget);
         
-        
         //Info icon
 		FileInputStream info2 = new FileInputStream("Images/information.png");
 		ImageIcon info = new ImageIcon(ImageIO.read(info2));
@@ -266,7 +279,7 @@ public class Interface extends JFrame{
 
 				frame.setTitle("CeSort - Question 1 ");
 				num_page = 1 ; 	//Initialize the num_page
-				jquestion.setText("Question "+num_page+" : "+questions.get_qu(num_page)); //Update the title 
+				jquestion.setText("Question "+num_page+" : "+questions.get_question(num_page)); //Update the title 
 				cardLayout.show(contentPane, "qus");
 
 			}
@@ -281,44 +294,7 @@ public class Interface extends JFrame{
 		});
 						
 
-		/*********************		Questions 	*******************/
-		
-		
-		//Home Button
-		JButton button_home = new JButton("Home");
-		button_home.setFont(new Font("Bitstream Charter", Font.BOLD, 25)); 	
-		button_home.setBounds(630, 470, 150,50);	
-		button_home.setForeground(Color.WHITE);
-		button_home.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
-		button_home.setBorder(BorderFactory.createLineBorder(Color.black));
-		qus_panel.add(button_home);
-		
-		//Previous Button
-		JButton button_previous = new JButton("Previous");
-		button_previous.setFont(new Font("Bitstream Charter", Font.BOLD, 25)); 	
-		button_previous.setBounds(50, 470, 150,50);	
-		button_previous.setForeground(Color.WHITE);
-		button_previous.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
-		button_previous.setBorder(BorderFactory.createLineBorder(Color.black));
-		qus_panel.add(button_previous);
-
-
-		//Next Button
-		JButton button_next = new JButton("Next");
-		button_next.setFont(new Font("Bitstream Charter", Font.BOLD, 30)); 	
-		button_next.setBounds(300, 470, 150,50);	
-		button_next.setForeground(Color.WHITE);
-		button_next.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
-		button_next.setBorder(BorderFactory.createLineBorder(Color.black));
-		qus_panel.add(button_next);
-
-
-		//Label CeSort title 
-		JLabel jcesort1 = new JLabel("CeSort");
-		jcesort1.setForeground(Color.DARK_GRAY);
-		jcesort1.setFont(new Font("Bitstream Charter", Font.BOLD , 60));
-		jcesort1.setBounds(40, 40, 300, 80);
-		qus_panel.add(jcesort1) ;
+		/*********************		Questions Panel     	*******************/
 		
 		//Logo CeSort
 		FileInputStream icone2 = new FileInputStream("Images/Logo-STAR.jpg");
@@ -330,6 +306,15 @@ public class Interface extends JFrame{
 		image2.setBounds(450,20,70,40);
         qus_panel.add(image2);
         
+        //Where the answer in insered
+		JTextField janswer = new JTextField("");
+		janswer.setForeground(Color.DARK_GRAY);
+		janswer.setFont(new Font("Bitstream Charter", Font.BOLD , 18));
+		janswer.setBounds(20, 200, 500, 80);
+		janswer.setBorder(BorderFactory.createLineBorder(Color.black));
+		qus_panel.add(janswer) ;
+		
+        
         //Previous choices title
 		JLabel jprevious = new JLabel("Your previous choices :");
 		jprevious.setForeground(Color.DARK_GRAY);
@@ -337,52 +322,46 @@ public class Interface extends JFrame{
 		jprevious.setBounds(600, 20, 300, 80);
 		qus_panel.add(jprevious) ;
 		
+		//Scroll panel with the previous questions+answers
+		JScrollPane jlist_answers = new JScrollPane() ;
+		jlist_answers.setForeground(Color.DARK_GRAY);
+		jlist_answers.setFont(new Font("Bitstream Charter", Font.BOLD , 20));
+		jlist_answers.setBounds(550, 100, 320, 360);
+		qus_panel.add(jlist_answers) ;
 		
-        
-		//Previous Button Listener
-		button_previous.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				//Test if there is a previous page
-				if (num_page>1) {
-					num_page -- ; 	//Decrement the num_page
-					jquestion.setText("Question "+num_page+" : "+questions.get_qu(num_page)); //Update the number and content of the question
-					frame.setTitle("CeSort - Question "+num_page); //Actualize the title frame
-					cardLayout.show(contentPane, "qus");
-					
-				}
-				else if (num_page==1) { //If we are already at the first page
-					
-					//Open a new error windows
-					//TO BE CONTINUED
-				}
-				
-			}
-		});
+		//Content of the scroll panel
+		JTextArea jta_list_answers = new JTextArea();
+		jlist_answers.setViewportView(jta_list_answers);
+		jta_list_answers.setLineWrap(true);
+		jta_list_answers.setWrapStyleWord(true);
+		jta_list_answers.setEditable(false);
+		jta_list_answers.setBorder(BorderFactory.createLineBorder(Color.black));
+		jta_list_answers.setFont(new Font("Bitstream Charter", Font.PLAIN, 19));
+
 		
-		//Next Button Listener
-		button_next.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		
+		
+		
+		
+		/*
+		//Previous choices title
+		JLabel jerror3 = new JLabel("TEST xxxxxxxxxxxxxxxxxxxx:");
+		jerror3.setForeground(Color.DARK_GRAY);
+		jerror3.setFont(new Font("Bitstream Charter", Font.BOLD , 20));
+		jerror3.setBounds(600, 100, 300, 80);
+		qus_panel.add(jerror3) ; */
 
-				
-				//Test if there is a next page 
-				if (num_page<nb_questions) {
-					num_page ++ ; 	//Increment the num_page
-					jquestion.setText("Question "+num_page+" : "+questions.get_qu(num_page)); //Update the title 
-					frame.setTitle("CeSort - Question "+num_page); //Actualize the title frame
-					cardLayout.show(contentPane, "qus");
-					
-				}
-				else { //If we are already at the first page
-					
-					//Open a new error windows
-					//TO BE CONTINUED
-				}
-				
-				
-
-			}
-		});
+		
+		// 		Buttons		//
+		
+		//Home Button
+		JButton button_home = new JButton("Home");
+		button_home.setFont(new Font("Bitstream Charter", Font.BOLD, 25)); 	
+		button_home.setBounds(630, 470, 150,50);	
+		button_home.setForeground(Color.WHITE);
+		button_home.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
+		button_home.setBorder(BorderFactory.createLineBorder(Color.black));
+		qus_panel.add(button_home);
 		
 		//Home Button Listener
 		button_home.addActionListener(new ActionListener() {
@@ -396,11 +375,129 @@ public class Interface extends JFrame{
 			}
 		});
 		
+		//Finish Button
+		JButton button_finish = new JButton("Finish");
+		button_finish.setFont(new Font("Bitstream Charter", Font.BOLD, 30)); 	
+		button_finish.setBounds(360, 470, 150,50);	
+		button_finish.setForeground(Color.WHITE);
+		button_finish.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
+		button_finish.setBorder(BorderFactory.createLineBorder(Color.black));
+		button_finish.setVisible(false);
+		qus_panel.add(button_finish);
+		
+		//Finish Button Listener --> Go to the results page
+		button_finish.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				num_page = 1 ; 	//Increment the num_page
+				frame.setTitle("CeSort - Results "); 			 
+				cardLayout.show(contentPane, "results");	
+
+			}
+		});
+
+		
+		//Next Button
+		JButton button_next = new JButton("Next");
+		button_next.setFont(new Font("Bitstream Charter", Font.BOLD, 30)); 	
+		button_next.setBounds(360, 470, 150,50);	
+		button_next.setForeground(Color.WHITE);
+		button_next.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
+		button_next.setBorder(BorderFactory.createLineBorder(Color.black));
+		qus_panel.add(button_next);
+
+		//Next Button Listener
+		button_next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				//Test if there is a next page 
+				if (num_page<(nb_questions-1)) {
+					
+					//We retrieve the answer 		
+					String answer = janswer.getText() ;
+					questions.set_answer(num_page, answer); //We save it
+					//We actualize the list of answer at the right of the screen
+					String Newline=System.getProperty("line.separator");
+					jta_list_answers.setText(jta_list_answers.getText()+questions.get_question(num_page)+Newline+answer+Newline+Newline);
+					
+					num_page ++ ; 	//Increment the num_page
+					jquestion.setText("Question "+num_page+" : "+questions.get_question(num_page)); //Update the title 
+					frame.setTitle("CeSort - Question "+num_page); //Actualize the title frame
+					cardLayout.show(contentPane, "qus");
+
+					
+				}
+				if (num_page==(nb_questions-1)) { //If there is no next question
+				
+					//We replace the next button with the finish button
+					button_next.setVisible(false);	
+					button_finish.setVisible(true);	
+				}	
+				janswer.setText("");  	//Clear the answer area
+
+			
+			}
+		});
+		
+		
+		//Previous Button
+		JButton button_previous = new JButton("Previous");
+		button_previous.setFont(new Font("Bitstream Charter", Font.BOLD, 25)); 	
+		button_previous.setBounds(50, 470, 150,50);	
+		button_previous.setForeground(Color.WHITE);
+		button_previous.setBackground(Color.getHSBColor(0.56f, 1.0f, 0.4f));
+		button_previous.setBorder(BorderFactory.createLineBorder(Color.black));
+		qus_panel.add(button_previous);
+		 
+		//Previous Button Listener
+		button_previous.addActionListener(new ActionListener() {
+			public void actionPerformed (ActionEvent e) {
+			
+			//Test if there is a previous page
+			if (num_page>1) {
+				num_page -- ; 	//Decrement the num_page
+				jquestion.setText("Question "+num_page+" : "+questions.get_question(num_page)); //Update the number and content of the question
+				frame.setTitle("CeSort - Question "+num_page); //Actualize the title frame
+				cardLayout.show(contentPane, "qus"); 
+				
+				//If the finish button is visible, we make sure that it's the next button instead
+				button_next.setVisible(true);	
+				button_finish.setVisible(false);
+				
+			}
+			else if (num_page==1) { //If we are already at the first page
+				
+				JOptionPane.showMessageDialog(null, "There is no previous question!", "Error", 0);
+			}
+			janswer.setText("");  	//Clear the answer area
+			
+			}
+		});
+
+
+
+		//Label CeSort title 
+		JLabel jcesort1 = new JLabel("CeSort");
+		jcesort1.setForeground(Color.DARK_GRAY);
+		jcesort1.setFont(new Font("Bitstream Charter", Font.BOLD , 60));
+		jcesort1.setBounds(40, 40, 300, 80);
+		qus_panel.add(jcesort1) ;
+		
+		
 		//Global
 		contentPane.add(welcome_panel, "welcome");
 		contentPane.add(qus_panel, "qus");
-		//contentPane.add(panel_modif, "modif");
+		contentPane.add(results_panel, "results");
 
+
+	}
+	
+	
+	public void actualization_answers() {
+		
+		for (int i=0; i<num_page; i++) {
+			
+		}
 	}
 
 }
